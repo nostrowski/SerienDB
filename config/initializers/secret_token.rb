97ca4757386c7ccb04be7c6a4737da86728a3cc0
Nils Ostrowski <fh@nijos.de>
2012-03-01 08:05:06 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SerienDB::Application.config.secret_token = '2a2e806f9595083f651d56dd3566a3b74b7f560b0e23ecc7bf99406b6bd608125d4fbcabaab29f1d0d0fa47113ca187dceed728688c02329d32cacafbb29d428'
