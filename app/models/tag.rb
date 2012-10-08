dcbcbd26ec3cc4581f703a65f292ad17b596adb8
Nils Ostrowski <home@nijos.de>
2012-10-08 20:29:13 +0200

    In der Tag-Prüfung werden nun auch Hex-Farbcodes zugelassen

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,7 +6,7 @@ class Tag < ActiveRecord::Base
   
   validates_length_of :acronym, :is => 1
   validates_inclusion_of :acronym, :in => 'A'..'Z'
-  validates_inclusion_of :color, :in => VALID_COLORS
+  validate :valid_color?
   
   def self.for_series
     Tag.find :all, :order => 'priority DESC', :conditions => ['seasontag = "0"']
@@ -14,6 +14,17 @@ class Tag < ActiveRecord::Base
   
   def self.for_seasons
     Tag.find :all, :order => 'priority DESC', :conditions => ['seasontag = "1"']
+  end
+  
+  def self.hex_color?(value)
+    return false unless value
+    /^#(?:[0-9a-f]{3})(?:[0-9a-f]{3})?$/i.match(value).nil? ? false : true
+  end
+  
+  def valid_color?
+    unless self.class.hex_color?(self.color) || VALID_COLORS.include?(self.color)
+      errors.add(:color, "Siehe Beschreibung")
+    end
   end
   
   def priority_up!
